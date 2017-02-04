897e000eb80eb9902897dd632d90f84bac92123e
r5on11c <ralfmathes.0@gmail.com>
2017-02-04 01:33:11 +0100

    Add clearChat function

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ var cors = require('cors');
 var app = express();
 var messages = [];
 
+var clearChat = function() {
+    messages = [];
+}
+
 app.use(cors());
 app.use(bodyParser.text());
 app.get('/messages', (req, res) => {
     res.send(messages);
 });
-
+app.post('/clearchat',(req, res) => {
+    clearChat();
+    res.sendStatus(200);
+});
 app.post('/messages', (req, res) => {
     messages.push (req.body);
     res.sendStatus(200);
